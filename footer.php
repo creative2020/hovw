b074d0938b5d87adca3f186d6eecec1fc4e441f6
Jeff Potts <jeff@2020creative.com>
2014-07-10 11:48:54 -0500

    start

---
+++ footer.php
@@ -0,0 +1,11 @@
+	<div class="alignleft">
+		<strong><?php bloginfo( 'name' ); ?></strong><br />
+Phone 417.501.myvw (6989)<BR>
+Email info@hovwspecialists.com<BR>
+		<?php printf( __( 'Copyright &copy; %s All Rights Reserved', 'it-l10n-Builder' ), date( 'Y' ) ); ?>
+	</div>
+	<div class="alignright">
+		<a href="http://www.2020creative.com" title="Website Design" target="_blank">Website Design</a></br>
+        Powered by <a href="http://www.2020creative.com" title="Website Design" target="_blank">2020 Creative</a>
+	</div>
+	<?php wp_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+// TT Functions
+require_once('tt-lib/tt-functions.php');
+
+add_theme_support( 'builder-3.0' );
+add_theme_support( 'builder-responsive' );
